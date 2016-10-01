308c632a4f98db843dc86edd302c50717fc111dc
Ilqar <ccmilgar@gmail.com>
2016-10-01 10:08:59 +0400

    Adding test class

---
+++ src/com/company/TestClass.java
@@ -0,0 +1,10 @@
+package com.company;
+
+/**
+ * Created by user on 10/1/2016.
+ */
+public class TestClass {
+    public void testMethod(){
+        System.out.println("Inside test method");
+    }
+}
